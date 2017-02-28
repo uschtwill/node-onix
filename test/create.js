bded76ea88d53b3e66bbb84184bfc1236b8dde2f
Christian Roy <roychristian@gmail.com>
2017-02-27 21:20:13 -0500

    Implement basic ONIX support

--- test/create.js
+++ test/create.js
@@ -4,11 +4,30 @@ var onix = require('../');
 describe('Creating', function () {
     it('should correctly create ONIX 3', function() {
         var xml = onix.create({
+          header: {
             sender: {
                 senderName: "Christian Roy",
                 emailAddress: "roychristian@gmail.com"
             },
             sentDateTime: new Date(2017, 2, 23)
+          },
+          products: [{
+            record: "record",
+            notification: 4,
+            description: {
+              title: {
+                type: 9,
+                element: {
+                  level: 2,
+                  text: 'Le Title'
+                }
+              },
+              language: {
+                role: 1,
+                code: 'fre'
+              }
+            }
+          }]
         }, '3.0');
 
         console.log(xml);
--- test/parse.js
+++ test/parse.js
@@ -17,10 +17,49 @@ describe('Parsing', function () {
 
 describe('Parsing ONIX 3', function() {
     var onix3sample = fs.readFileSync(path.join(__dirname, './fixtures/onix3sample.xml'), { encoding: 'utf-8' });
-    var feed;
+    var o3feed;
+    o3feed = onix.parse(onix3sample, '3.0');
 
     it('should correctly parse ONIX 3 xml', function() {
-        feed = onix.parse(onix3sample, '3.0');
-        console.log(JSON.stringify(feed, null, 4));
+        o3feed.header.sender.name.should.eql('Sender Name');
+        o3feed.products[0].description.title.element.text.should.eql('The Title');
+    });
+
+    it('should find the product ISBN', function() {
+      o3feed.products[0].ids[0].value.should.eql('978XXXXXXXXXX');
+    });
+
+    it('should find the product author', function() {
+      o3feed.products[0].description.contributors[0].name.should.eql('AuthorFirst AuthorLast');
+      o3feed.products[0].description.contributors[0].note.should.eql('bio notes');
+    });
+
+    it('should find the product publisher name', function() {
+      o3feed.products[0].publishingDetail.publisher.name.should.eql('The Publisher');
+    });
+
+    it('should find the product publishing status', function() {
+      o3feed.products[0].publishingDetail.status.should.eql(4);
+    });
+
+    it('should find the product availability', function() {
+      o3feed.products[0].productSupply[0].details[0].availability.should.eql(20);
+    });
+
+    it('should find the product supply dates', function() {
+      o3feed.products[0].productSupply[0].details[0].dates[0].role.should.eql(8);
+    });
+
+    it('should find the product prices', function() {
+      prices = o3feed.products[0].productSupply[0].details[0].prices;
+      prices.length.should.eql(45);
+      prices[0].type.should.eql(4);
+      prices[0].qualifier.should.eql(5);
+      prices[0].currency.should.eql('EUR');
+      prices[0].amount.should.eql(13.99);  // '13,99' in the ONIX file
+      prices[1].amount.should.eql(13.99);  // '13.99' in the ONIX file
+      prices[0].territory.countries.should.eql('BG');
+      prices[2].dates[0].role.should.eql(14);
+      prices[2].dates[0].date.should.eql(new Date(2015, 4, 27))
     });
 });

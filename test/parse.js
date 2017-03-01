bc7e583fd5297f344128443be256aa6e0c008f2e
Christian Roy <roychristian@gmail.com>
2017-03-01 11:22:36 -0500

    Add support for Market/SalesRestrictinons, MarketPublishingStatus and MarketDate

--- test/parse.js
+++ test/parse.js
@@ -19,39 +19,40 @@ describe('Parsing ONIX 3', function() {
     var onix3sample = fs.readFileSync(path.join(__dirname, './fixtures/onix3sample.xml'), { encoding: 'utf-8' });
     var o3feed;
     o3feed = onix.parse(onix3sample, '3.0');
+    var product = o3feed.products[0];
 
     it('should correctly parse ONIX 3 xml', function() {
         o3feed.header.sender.name.should.eql('Sender Name');
-        o3feed.products[0].description.title.element.text.should.eql('The Title');
+        product.description.title.element.text.should.eql('The Title');
     });
 
     it('should find the product ISBN', function() {
-      o3feed.products[0].ids[0].value.should.eql('978XXXXXXXXXX');
+      product.ids[0].value.should.eql('978XXXXXXXXXX');
     });
 
     it('should find the product author', function() {
-      o3feed.products[0].description.contributors[0].name.should.eql('AuthorFirst AuthorLast');
-      o3feed.products[0].description.contributors[0].note.should.eql('bio notes');
+      product.description.contributors[0].name.should.eql('AuthorFirst AuthorLast');
+      product.description.contributors[0].note.should.eql('bio notes');
     });
 
     it('should find the product publisher name', function() {
-      o3feed.products[0].publishingDetail.publisher.name.should.eql('The Publisher');
+      product.publishingDetail.publisher.name.should.eql('The Publisher');
     });
 
     it('should find the product publishing status', function() {
-      o3feed.products[0].publishingDetail.status.should.eql(4);
+      product.publishingDetail.status.should.eql(4);
     });
 
     it('should find the product availability', function() {
-      o3feed.products[0].productSupply[0].details[0].availability.should.eql(20);
+      product.productSupply[0].details[0].availability.should.eql(20);
     });
 
     it('should find the product supply dates', function() {
-      o3feed.products[0].productSupply[0].details[0].dates[0].role.should.eql(8);
+      product.productSupply[0].details[0].dates[0].role.should.eql(8);
     });
 
     it('should find the product prices', function() {
-      prices = o3feed.products[0].productSupply[0].details[0].prices;
+      prices = product.productSupply[0].details[0].prices;
       prices.length.should.eql(45);
       prices[0].type.should.eql(4);
       prices[0].qualifier.should.eql(5);
@@ -61,5 +62,21 @@ describe('Parsing ONIX 3', function() {
       prices[0].territory.countries.should.eql('BG');
       prices[2].dates[0].role.should.eql(14);
       prices[2].dates[0].date.should.eql(new Date(2015, 4, 27))
+    });
+
+    it('should find sales restrictions for product supplies', function() {
+      noRestrictions = product.productSupply[0].market[0].salesRestrictions;
+      restrictions = product.productSupply[1].market[0].salesRestrictions;
+      should.not.exist(noRestrictions);
+      restrictions[0].type.should.eql(6);
+    });
+
+    it('should find the market publishing status', function() {
+      product.productSupply[0].marketPublishingDetail[0].status.should.eql(4);
+    });
+
+    it('should find the market date', function() {
+      product.productSupply[0].marketPublishingDetail[0].dates[0].role.should.eql(1);
+      product.productSupply[0].marketPublishingDetail[0].dates[0].date.should.eql(new Date(2015, 4, 27));
     });
 });

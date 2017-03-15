c23e9919ce830202c2440eb9446b3155a84c1e8d
Christian Roy <roychristian@gmail.com>
2017-03-15 14:49:11 -0400

    Support long date format with hours, mins and secs

--- test/utils.js
+++ test/utils.js
@@ -4,8 +4,17 @@ var should = require('should');
 describe('Utils', function () {
     it('should correctly format a date', function() {
         var d = new Date(2015, 05, 1);
-
         utils.formatDate(d).should.be.equal('20150601');
+    });
+
+    it('should correctly unformat a date', function() {
+        var d = utils.unformatDate('20170315').toString();
+        d.valueOf().should.be.equal(new Date(2017, 2, 15).toString());
+    });
+
+    it('should correctly unformat a date with time', function() {
+        var d = utils.unformatDate('20170228T162554Z').toString();
+        d.valueOf().should.be.equal(new Date(2017, 1, 28, 16, 25, 54).toString());
     });
 
     it('should correctly pad integer', function() {
